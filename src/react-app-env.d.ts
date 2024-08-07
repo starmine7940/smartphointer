93bc55f2ae1e4f2202e5fe42959e9ab908535eb7
Takumi Goto <takumi.goto@sansan.com>
2024-08-07 20:12:40 +0900

    DeviceMotionEventWithRequestPermission を定義

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,4 @@
 /// <reference types="react-scripts" />
+interface DeviceMotionEventWithRequestPermission extends DeviceMotionEvent {
+    requestPermission?: () => Promise<'granted' | 'denied'>;
+}
